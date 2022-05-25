b55dacb4473e3d608b7d3f1dc0777f9a2503ce08
Adinda Ayu Azzahra <4519210056@univpancasila.ac.id>
2022-05-25 21:27:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/gobooks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gobooks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/styles.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final Color accentColor = Color(0xFF413B89);
+final Color secdarkColor = Color(0xFFA99153);
+final Color secLightColor = Color(0xFFF0E5C8);
+final Color bgColor = Color(0xFFFFFFFF);
+
+final TextTheme myTextTheme = TextTheme(
+  headline1: GoogleFonts.poppins(
+      fontSize: 93, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.poppins(
+      fontSize: 58, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.poppins(fontSize: 46, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.poppins(
+      fontSize: 33, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.poppins(
+      fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.poppins(
+      fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.poppins(
+      fontSize: 13, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.outfit(
+      fontSize: 18, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.outfit(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.outfit(
+      fontSize: 16, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.outfit(
+      fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.outfit(
+      fontSize: 11, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
