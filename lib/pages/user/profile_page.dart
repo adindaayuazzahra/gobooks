ad8bbb3978b26fcfaaf6360da887d8144798872f
Adinda Ayu Azzahra <4519210056@univpancasila.ac.id>
2022-06-26 19:52:31 +0700

    memperbaiki halaman profile

--- lib/pages/user/profile_page.dart
+++ lib/pages/user/profile_page.dart
@@ -10,7 +10,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    //Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
@@ -28,15 +28,16 @@ class _ProfilePageState extends State<ProfilePage> {
           onTap: () => FocusScope.of(context).unfocus(),
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Row(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  const SizedBox(height: 10),
                   Container(
-                    width: size.width * 0.9,
+                    width: 320,
                     height: 200,
                     decoration: BoxDecoration(
                       color: accentColor,
@@ -46,7 +47,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
                       child: Column(
                         mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text('Dicoding Indonesia',
                               style: Theme.of(context)
@@ -63,155 +64,128 @@ class _ProfilePageState extends State<ProfilePage> {
                             style: TextStyle(color: Colors.white),
                           ),
                           const SizedBox(height: 20),
-                          ElevatedButton.icon(
-                            style: ElevatedButton.styleFrom(
-                                primary: secLightColor,
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 13, horizontal: 70),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20))),
-                            onPressed: () async {
-                              await AuthServices.signOut();
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const WelcomePage()),
-                              );
-                            },
-                            icon: const Icon(
-                              Icons.logout_rounded,
-                              color: Colors.black,
-                              size: 25,
-                            ),
-                            label: Text(
-                              'Log Out',
-                              style:
-                                  Theme.of(context).textTheme.button?.copyWith(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                      ),
-                            ),
-                          ),
-                          // Padding(
-                          //   padding:
-                          //       EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
-                          //   child: FFButtonWidget(
-                          //     onPressed: () {
-                          //       print('Button pressed ...');
-                          //     },
-                          //     text: 'LOG OUT',
-                          //     icon: Icon(
-                          //       Icons.logout,
-                          //       size: 15,
-                          //     ),
-                          //     options: FFButtonOptions(
-                          //       width: double.infinity,
-                          //       height: 50,
-                          //       color: Color(0xFFF0E5C8),
-                          //       style: Theme.of(context)
-                          //           .textTheme
-                          //           .headline6
-                          //           ?.copyWith(color: accentColor),
-                          //       borderSide: BorderSide(
-                          //         color: Colors.transparent,
-                          //         width: 1,
-                          //       ),
-                          //       borderRadius: 50,
-                          //     ),
-                          //   ),
-                          // ),
                         ],
                       ),
                     ),
                   ),
                 ],
               ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: () async {
+                  await AuthServices.signOut();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const WelcomePage()),
+                  );
+                },
+                child: Container(
+                  height: 55,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: secLightColor,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.logout_rounded,
+                        color: Colors.black,
+                        size: 25,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'Log Out',
+                        style: Theme.of(context).textTheme.button?.copyWith(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
-      // body: SingleChildScrollView(
-      //   child: Padding(
-      //     padding: const EdgeInsets.fromLTRB(15, 20, 15, 30),
+
+      // body: SafeArea(
+      //   child: GestureDetector(
+      //     onTap: () => FocusScope.of(context).unfocus(),
       //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       mainAxisSize: MainAxisSize.max,
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       //       crossAxisAlignment: CrossAxisAlignment.center,
-      //       children: <Widget>[
-      //         Container(
-      //           width: size.width * 1,
-      //           padding: const EdgeInsets.symmetric(vertical: 25.0),
-      //           decoration: BoxDecoration(
-      //             color: Colors.white,
-      //             borderRadius: BorderRadius.circular(10),
-      //             boxShadow: [
-      //               BoxShadow(
-      //                 color: Colors.grey.withOpacity(0.3),
-      //                 spreadRadius: 1,
-      //                 blurRadius: 3,
-      //                 offset: const Offset(2, 3),
+      //       children: [
+      //         Row(
+      //           mainAxisSize: MainAxisSize.max,
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             Container(
+      //               width: size.width * 0.9,
+      //               height: 200,
+      //               decoration: BoxDecoration(
+      //                 color: accentColor,
+      //                 borderRadius: BorderRadius.circular(20),
       //               ),
-      //             ],
-      //           ),
-      //           child: Column(
-      //             children: [
-      //               Text('DICODING Indonesia',
-      //                   style: Theme.of(context)
-      //                       .textTheme
-      //                       .headline6
-      //                       ?.copyWith(color: accentColor)
-      //               ),
-      //               const Text('Status : Mahasiswa',
-      //                   style: TextStyle(
-      //                     fontWeight: FontWeight.w500,
-      //                   )),
-      //               const Text('0812344566655'),
-      //               const SizedBox(height: 20),
-      //               ElevatedButton.icon(
-      //                 style: ElevatedButton.styleFrom(
-      //                     primary: secdarkColor,
-      //                     padding: const EdgeInsets.symmetric(
-      //                         vertical: 6, horizontal: 20
+      //               child: Padding(
+      //                 padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
+      //                 child: Column(
+      //                   mainAxisSize: MainAxisSize.max,
+      //                   mainAxisAlignment: MainAxisAlignment.end,
+      //                   children: [
+      //                     Text('Dicoding Indonesia',
+      //                         style: Theme.of(context)
+      //                             .textTheme
+      //                             .headline6
+      //                             ?.copyWith(color: Colors.white)),
+      //                     const Text('Status : Mahasiswa',
+      //                         style: TextStyle(
+      //                           fontWeight: FontWeight.w500,
+      //                           color: Colors.white,
+      //                         )),
+      //                     const Text(
+      //                       '0812344566655',
+      //                       style: TextStyle(color: Colors.white),
       //                     ),
-      //                     shape: RoundedRectangleBorder(
-      //                         borderRadius: BorderRadius.circular(20)
-      //                     )
-      //                 ),
-      //                 onPressed: () async {
-      //                   await AuthServices.signOut();
-      //                   Navigator.push(context,
-      //                     MaterialPageRoute(
-      //                       builder: (context) => const WelcomePage()
-      //                     ),
-      //                   );
-      //                 },
-      //                 icon: const Icon(
-      //                   Icons.logout_rounded,
-      //                   color: Colors.white,
-      //                   size: 25,
-      //                 ),
-      //                 label: Text(
-      //                   'Log Out',
-      //                   style: Theme.of(context).textTheme.button?.copyWith(
-      //                         fontSize: 20,
-      //                         color: Colors.white,
+      //                     const SizedBox(height: 20),
+      //                     ElevatedButton.icon(
+      //                       style: ElevatedButton.styleFrom(
+      //                           primary: secLightColor,
+      //                           padding: const EdgeInsets.symmetric(
+      //                               vertical: 13, horizontal: 70),
+      //                           shape: RoundedRectangleBorder(
+      //                               borderRadius: BorderRadius.circular(20))),
+      //                       onPressed: () async {
+      //                         await AuthServices.signOut();
+      //                         Navigator.push(
+      //                           context,
+      //                           MaterialPageRoute(
+      //                               builder: (context) => const WelcomePage()),
+      //                         );
+      //                       },
+      //                       icon: const Icon(
+      //                         Icons.logout_rounded,
+      //                         color: Colors.black,
+      //                         size: 25,
       //                       ),
+      //                       label: Text(
+      //                         'Log Out',
+      //                         style:
+      //                             Theme.of(context).textTheme.button?.copyWith(
+      //                                   fontSize: 20,
+      //                                   color: Colors.black,
+      //                                 ),
+      //                       ),
+      //                     ),
+      //                   ],
       //                 ),
       //               ),
-      //               ElevatedButton(
-      //                 onPressed: () {
-      //                   Navigator.push(context,
-      //                       MaterialPageRoute(builder:
-      //                           (context) => const MainPageAdmin()
-      //                       )
-      //                   );
-      //                 },
-      //                 child: Container(
-      //                   color: secdarkColor,
-      //                   child: const Text('Page untuk Admin'),
-      //                 ),
-      //               ),
-      //             ],
-      //           ),
+      //             ),
+      //           ],
       //         ),
       //       ],
       //     ),
