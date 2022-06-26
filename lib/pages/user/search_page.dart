e3f2b70cee1952c799b82be5fe141c6a8e3f1b57
Adinda Ayu Azzahra <4519210056@univpancasila.ac.id>
2022-06-26 15:54:25 +0700

    memperbaiki halaman pencarian

--- lib/pages/user/search_page.dart
+++ lib/pages/user/search_page.dart
@@ -9,7 +9,8 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  final CollectionReference _books = FirebaseFirestore.instance.collection('Book');
+  final CollectionReference _books =
+      FirebaseFirestore.instance.collection('Book');
   final TextEditingController _searchControl = TextEditingController();
   String queries = '';
 
@@ -27,92 +28,89 @@ class _SearchPageState extends State<SearchPage> {
                 .headline6
                 ?.copyWith(color: accentColor, fontSize: 25),
           ),
-
-          flexibleSpace: Column(
-              children: [
-                const SizedBox(
-                  height: 70,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15), 
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10), 
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 1), 
-                    decoration: BoxDecoration(
-                      color: Colors.white, 
-                      borderRadius: BorderRadius.circular(30), 
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3), 
-                          spreadRadius: 1, 
-                          blurRadius: 3, 
-                          offset: const Offset(2, 3),
-                        ),
-                      ],
-                    ), 
-                    child: TextField(
-                      cursorColor: Colors.black, 
-                      decoration: const InputDecoration(
-                        icon: Icon(
-                          Icons.search_rounded, 
-                          color: secdarkColor,
-                        ), 
-                        hintText: "Cari Judul/Penulis/Kategori", 
-                        border: InputBorder.none,
-                      ), 
-                      onChanged: (value) {
-                        setState(() {
-                          queries = value;
-                        });
-                        },
+          flexibleSpace: Column(children: [
+            const SizedBox(
+              height: 70,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                      offset: const Offset(2, 3),
                     ),
-                  ),
+                  ],
                 ),
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: const InputDecoration(
+                    icon: Icon(
+                      Icons.search_rounded,
+                      color: secdarkColor,
+                    ),
+                    hintText: "Cari Judul atau Penulis",
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      queries = value;
+                    });
+                  },
+                ),
+              ),
+            ),
           ]),
         ),
         body: StreamBuilder(
           stream: _books.snapshots(),
           builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-            if(streamSnapshot.hasData){
+            if (streamSnapshot.hasData) {
               return ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: streamSnapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final DocumentSnapshot documentSnapshot = streamSnapshot
-                      .data!.docs[index];
-                  if (queries.isEmpty) {
-                    return const Center();
-                  }
-                  if (documentSnapshot['bookTitle']
-                      .toString()
-                      .toLowerCase()
-                      .contains(queries.toLowerCase()) ||
-                      documentSnapshot['bookTitle']
-                          .toString()
-                          .toLowerCase()
-                          .startsWith(queries.toLowerCase())) {
-                    return BookList(documentSnapshot: documentSnapshot);
-                  } else if (documentSnapshot['bookAuthor']
-                      .toString()
-                      .toLowerCase()
-                      .contains(queries.toLowerCase()) ||
-                      documentSnapshot['bookTitle']
-                          .toString()
-                          .toLowerCase()
-                          .startsWith(queries.toLowerCase())) {
-                    return BookList(documentSnapshot: documentSnapshot);
-                  } else {
-                    return const Center();
-                  }
-                }
-              );
+                  scrollDirection: Axis.vertical,
+                  itemCount: streamSnapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot documentSnapshot =
+                        streamSnapshot.data!.docs[index];
+                    if (queries.isEmpty) {
+                      return const Center();
+                    }
+                    if (documentSnapshot['bookTitle']
+                            .toString()
+                            .toLowerCase()
+                            .contains(queries.toLowerCase()) ||
+                        documentSnapshot['bookTitle']
+                            .toString()
+                            .toLowerCase()
+                            .startsWith(queries.toLowerCase())) {
+                      return BookList(documentSnapshot: documentSnapshot);
+                    } else if (documentSnapshot['bookAuthor']
+                            .toString()
+                            .toLowerCase()
+                            .contains(queries.toLowerCase()) ||
+                        documentSnapshot['bookTitle']
+                            .toString()
+                            .toLowerCase()
+                            .startsWith(queries.toLowerCase())) {
+                      return BookList(documentSnapshot: documentSnapshot);
+                    } else {
+                      return const Center();
+                    }
+                  });
             }
             return const Center(
               child: CircularProgressIndicator(color: accentColor),
             );
           },
-        )
-    );
+        ));
   }
 
   @override
