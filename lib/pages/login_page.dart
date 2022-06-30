9eb2183ca2a8a2d739010a471df3e9b616f94c75
Adinda Ayu Azzahra <4519210056@univpancasila.ac.id>
2022-06-30 16:22:44 +0700

    memperbaiki auth login admin

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -83,9 +83,7 @@ class LoginPage extends StatelessWidget {
                   child: ElevatedButton(
                     onPressed: () async {
                       final result = await AuthServices.signIn(
-                          emailController.text,
-                          passwordController.text
-                      );
+                          emailController.text, passwordController.text);
                       if (result != null) {
                         Navigator.pushReplacement(
                           context,
@@ -93,21 +91,20 @@ class LoginPage extends StatelessWidget {
                             builder: (context) => const MainPage(),
                           ),
                         );
-                      } else if(emailController.text == ""
-                          || passwordController.text == "") {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Email / Password tidak boleh kosong.'),
-                              duration: Duration(milliseconds: 600),
-                            )
-                        );
+                      } else if (emailController.text == "" ||
+                          passwordController.text == "") {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Email / Password tidak boleh kosong.'),
+                          duration: Duration(milliseconds: 600),
+                        ));
                       } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Email / Password salah. Silakan coba lagi.'),
-                              duration: Duration(milliseconds: 600),
-                            )
-                        );
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                              'Email / Password salah. Silakan coba lagi.'),
+                          duration: Duration(milliseconds: 600),
+                        ));
                       }
                     },
                     style: ElevatedButton.styleFrom(
@@ -136,31 +133,29 @@ class LoginPage extends StatelessWidget {
                   child: ElevatedButton(
                     onPressed: () async {
                       final result = await AuthServices.signIn(
-                          emailController.text,
-                          passwordController.text
-                      );
-                      if (result != null) {
+                          emailController.text, passwordController.text);
+                      if (emailController.text == "agustianbrian25@gmail.com" ||
+                          passwordController.text == "admin123") {
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
                             builder: (context) => const AdminProfilePage(),
                           ),
                         );
-                      } else if(emailController.text == ""
-                          || passwordController.text == "") {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Email / Password tidak boleh kosong.'),
-                              duration: Duration(milliseconds: 600),
-                            )
-                        );
+                      } else if (emailController.text == "" ||
+                          passwordController.text == "") {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Email / Password tidak boleh kosong.'),
+                          duration: Duration(milliseconds: 600),
+                        ));
                       } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Email / Password salah. Silakan coba lagi.'),
-                              duration: Duration(milliseconds: 600),
-                            )
-                        );
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text('Email Anda Tidak Terdaftar Sebagai Admin.'),
+                          duration: Duration(milliseconds: 600),
+                        ));
                       }
                     },
                     style: ElevatedButton.styleFrom(
@@ -189,15 +184,15 @@ class LoginPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(7),
                   child: OutlinedButton(
                     onPressed: () async {
-                       final result = await AuthServices.signUpWithGoogle();
-                       if (result != null) {
-                         Navigator.pushReplacement(
-                           context,
-                           MaterialPageRoute(
-                             builder: (context) => const MainPage(),
-                           ),
-                         );
-                       }
+                      final result = await AuthServices.signUpWithGoogle();
+                      if (result != null) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MainPage(),
+                          ),
+                        );
+                      }
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -212,8 +207,7 @@ class LoginPage extends StatelessWidget {
                             style: Theme.of(context)
                                 .textTheme
                                 .button
-                                ?.copyWith(color: Colors.black, fontSize: 13)
-                        ),
+                                ?.copyWith(color: Colors.black, fontSize: 13)),
                       ],
                     ),
                   ),
